3d58ab3c96604ecbc69c4058f26f0da0a1076ca1
minhaj-boop <imminhajj@gmail.com>
2024-01-07 22:20:59 +0600

    pages

---
+++ src/app/outlets/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OutletsPage = () => {
+  return <div>OutletsPage</div>;
+};
+
+export default OutletsPage;
---
+++ src/app/shoponline/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShoponlinePage = () => {
+  return <div>ShoponlinePage</div>;
+};
+
+export default ShoponlinePage;
